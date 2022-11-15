6b0695aedd0dcfd0d675b9ff182d14622d50171b
Neelakantan K.K <neelakantankk@gmail.com>
2022-11-16 00:47:56 +0530

    Fixed formatting using cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -2,19 +2,15 @@ pub mod employees {
 
     use std::collections::HashMap;
 
-    pub fn add_employee(employees: &mut HashMap<String,Vec<String>>, words: &Vec<&str>) {
+    pub fn add_employee(employees: &mut HashMap<String, Vec<String>>, words: &Vec<&str>) {
         for (index, &word) in words.iter().enumerate() {
             if word == "to" {
+                if index < words.len() - 1 {
+                    let employees_in_dep =
+                        employees.entry(words[index + 1..].join(" ")).or_default();
 
-                if index< words.len()-1 {
-                    let employees_in_dep = employees
-                        .entry(words[index+1..].join(" "))
-                        .or_default();
-
-                    employees_in_dep
-                        .push(words[1..index].join(" "));
+                    employees_in_dep.push(words[1..index].join(" "));
                     employees_in_dep.sort();
-
                 } else {
                     println!("Department not given!");
                 }
@@ -22,25 +18,23 @@ pub mod employees {
         }
     }
 
-    fn list_in_department(employees: &HashMap<String, Vec<String>>,
-        department_to_list : String) {
+    fn list_in_department(employees: &HashMap<String, Vec<String>>, department_to_list: String) {
         match employees.get(&department_to_list) {
-            None => println!("No employees found for {}",department_to_list),
+            None => println!("No employees found for {}", department_to_list),
             Some(list) => {
-                println!("-----{}-----",department_to_list);
+                println!("-----{}-----", department_to_list);
                 for name in list {
-                    println!("{}",name);
+                    println!("{}", name);
                 }
             }
         }
-
     }
 
     fn list_all_by_department(employees: &HashMap<String, Vec<String>>) {
-        for (department,names) in employees {
-            println!("-------{}--------",department);
+        for (department, names) in employees {
+            println!("-------{}--------", department);
             for name in names {
-                println!("\t{}",name);
+                println!("\t{}", name);
             }
         }
     }
@@ -54,9 +48,8 @@ pub mod employees {
         } else {
             match words[1] {
                 "Company" | "company" => list_all_by_department(employees),
-                _ => list_in_department(employees,words[1..].join(" "))
+                _ => list_in_department(employees, words[1..].join(" ")),
             }
         }
     }
-
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
-use std::io;
-use std::collections::HashMap;
 use add_employee_names::employees;
+use std::collections::HashMap;
+use std::io;
 
 fn main() {
-
-    let mut employees : HashMap<String, Vec<String>>  = HashMap::new();
+    let mut employees: HashMap<String, Vec<String>> = HashMap::new();
     loop {
         println!("What would you like to do?");
         println!("(Type 'Help' to see available commands.)");
@@ -15,14 +14,11 @@ fn main() {
             .expect("Failed to read line!");
 
         let words = input.split_whitespace().collect::<Vec<_>>();
-        
+
         match words.first() {
-            Some(&"add") | Some(&"Add") => 
-                employees::add_employee(&mut employees,&words),
-            Some(&"List") | Some(&"list") => 
-                employees::list_employees(&mut employees,&words),
-            Some(&"Quit") | Some(&"quit") | Some(&"q") =>
-                break,
+            Some(&"add") | Some(&"Add") => employees::add_employee(&mut employees, &words),
+            Some(&"List") | Some(&"list") => employees::list_employees(&mut employees, &words),
+            Some(&"Quit") | Some(&"quit") | Some(&"q") => break,
             Some(&"Help") | Some(&"help") => {
                 println!("Valid commands:");
                 println!("\tAdd <Employee Name> to <Department Name>");
@@ -30,7 +26,7 @@ fn main() {
                 println!("\tList Company");
                 println!("\tHelp");
                 println!("\tQuit");
-            },
+            }
             None => continue,
             Some(&&_) => {
                 println!("Unknown input");
@@ -40,9 +36,7 @@ fn main() {
                 println!("\tList Company");
                 println!("\tHelp");
                 println!("\tQuit");
-            },
+            }
         };
-
     }
 }
-
